8b6f6da9d9e9cbbee58b7dd4f5b534040771613a
DmitryKozin <44357314+DmitryKozin@users.noreply.github.com>
2020-06-03 14:42:38 +0700

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+// Вычисление суммы чисел
+int CalcSumm(int number)
+{
+	if (number < 10) 
+		return number;
+	return number % 10 + CalcSumm(number / 10);
+}
+// Вычисление предельного значения
+int CalcBounb(int a, int b, int bound, int increment)
+{
+	int statr_a = a;
+	int statr_b = b;
+	bool run = true;
+	int valABound;
+	int valBBound = CalcSumm(statr_b);
+	while (run)
+	{
+		valABound = CalcSumm(statr_a);
+		if (valABound + valBBound < bound)
+			statr_a += increment;
+		else
+			run = false;
+	}
+	return statr_a;
+}
+// Вычисление точек в секторе
+int CalcPointInSector(int a, int b)
+{
+	if (a != b)
+		return (((a + 1) * (b + 1)) / 2) + 1;
+	else
+		return ((a * a  + 3 * a) / 2) + 1;
+}
+
+int main()
+{
+	int x;
+	int y;
+	int bound;
+	while (true)
+	{
+		cout << "Insert X: ";
+		cin >> x;
+		//cout << CalcSumm(x)<<endl;
+		cout << "Insert Y: ";
+		cin >> y;
+		//cout << CalcSumm(y)<<endl;
+		cout << "Insert Bound: ";
+		cin >> bound;
+		//cout << "Max X: " << CalcBounb(x, y, bound, 1) << endl;
+		//cout << "Min X: " << CalcBounb(x, y, bound, -1) << endl;
+		//cout << "Max X - Min X: " << (CalcBounb(x, y, bound, 1) - CalcBounb(x, y, bound, -1)) << endl;
+		//cout << "Max Y: " << CalcBounb(y, x, bound, 1) << endl;
+		//cout << "Min Y: " << CalcBounb(y, x, bound, -1) << endl;
+		//cout << "Max Y - Min Y: " << (CalcBounb(y, x, bound, 1) - CalcBounb(y, x, bound, -1)) << endl;
+		cout << "Count of Point: " << CalcPointInSector((CalcBounb(x, y, bound, 1) - CalcBounb(x, y, bound, -1)), (CalcBounb(y, x, bound, 1) - CalcBounb(y, x, bound, -1))) << endl;
+	}
+	return 0;
+}
+
+
